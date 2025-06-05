63366b9285d36666335f7ba9629eecaa952ddc88
FogozIV <fogoziv@gmail.com>
2025-06-05 10:00:17 +0200

    Seems to be okay

--- src/utils/PacketDispatcher.cpp
+++ src/utils/PacketDispatcher.cpp
@@ -5,7 +5,7 @@
 #include "../../include/utils/PacketDispatcher.h"
 
 
-void PacketDispatcher::dispatch(std::shared_ptr<IPacket> packet) {
+void PacketDispatcher::dispatch(const std::shared_ptr<IPacket>& packet) {
     auto cbs = callbacks[static_cast<PacketType>(packet->getPacketID())];
     for (auto it = cbs.begin(); it != cbs.end(); ) {
         if (it->second(packet)) {
